7a2c44606ac37ca23e06a6028c232039e278daca
mayraaoteo <162721507+mayraaoteo@users.noreply.github.com>
2024-05-24 11:38:57 -0300

    Mandoo tuples.py

--- tuples.py
+++ tuples.py
@@ -1,32 +1,24 @@
-"""Functions to help Azara and Rui locate pirate treasure."""
-
-
 def get_coordinate(record):
-    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
-
-    :param record: tuple - with a (treasure, coordinate) pair.
-    :return: str - the extracted map coordinate.
-    """
-
-    return ""
+    return record[1]
 
 
 def convert_coordinate(coordinate):
-    """Split the given coordinate into tuple containing its individual components.
-
-    :param coordinate: str - a string map coordinate
-    :return: tuple - the string coordinate split into its individual components.
-    """
-
-    return ()
+    num = coordinate[0]
+    let = coordinate[1]
+    return (num, let)
 
 
 def create_record(azara_record, rui_record):
-    """Combine the two record types (if possible) and create a combined record group.
 
-    :param azara_record: tuple - a (treasure, coordinate) pair.
-    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
-    :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
-    """
+    a, b = azara_record
+    c, d, e = rui_record
+    num = b[0]
+    let = b[1]
+    coor_az = (num, let)
 
-    return ()
+    if coor_az == d:
+        final = azara_record + rui_record
+    else:
+        final = "not a match"
+
+    return final
